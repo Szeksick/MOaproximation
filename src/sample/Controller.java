98cd8a379f1a740a99d4072f7dc6128b4215f7d1
Szeksick <konradgugala@gmail.com>
2019-04-07 09:45:28 +0200

    First commit, app based of previous project MOinterpolation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -19,7 +19,7 @@ public class Controller {
 @FXML private Label filename;
 @FXML private AnchorPane ap;
 private ArrayList<Double> tabx, taby, templist;
-private ArrayList<ArrayList<Double>> subresult;
+private ArrayList<ArrayList<Double>> matrixS, matrixT;
 private Double h;
 
 
@@ -49,28 +49,41 @@ private Double h;
                 taby = new ArrayList<Double>(templist.subList(templist.size()/2,templist.size()));
                 templist.clear();
                 h = tabx.get(1) - tabx.get(0);
-                subresult = new ArrayList<ArrayList<Double>>();
+                matrixS = new ArrayList<ArrayList<Double>>();
                 for(int i = 0; i<tabx.size()-1; i++) {
-                    subresult.add(new ArrayList<Double>());
+                    matrixS.add(new ArrayList<Double>());
                     if(i==0){
                         for (int j = 1; j <tabx.size(); j++) {
-                            subresult.get(i).add(taby.get(j)-taby.get(j-1));
+                            matrixS.get(i).add(taby.get(j)-taby.get(j-1));
                         }
                     }else {
                         for (int j = 1; j <tabx.size() - i; j++) {
-                            subresult.get(i).add(subresult.get(i-1).get(j)-subresult.get(i-1).get(j-1));
+                            matrixS.get(i).add(matrixS.get(i-1).get(j)-matrixS.get(i-1).get(j-1));
+                        }
+                    }
+                }
+                matrixT = new ArrayList<ArrayList<Double>>();
+                for(int i = 0; i<tabx.size()-1; i++) {
+                    matrixT.add(new ArrayList<Double>());
+                    if(i==0){
+                        for (int j = 1; j <tabx.size(); j++) {
+                            matrixT.get(i).add(taby.get(j)-taby.get(j-1));
+                        }
+                    }else {
+                        for (int j = 1; j <tabx.size() - i; j++) {
+                            matrixT.get(i).add(matrixT.get(i-1).get(j)-matrixT.get(i-1).get(j-1));
                         }
                     }
                 }
 
-                resultout.setText("W"+String.valueOf(tabx.size()-1)+"(x)=");
-                resultout.appendText(String.valueOf(taby.get(0)));
-                for(int i = 0; i<tabx.size()-1; i++) {
-                    resultout.appendText(String.format("%s%.1f%s%d%s%.1f%s%d%s", "+(", subresult.get(i).get(0), "/(",i+1,"!*",h,"^",i+1,"))"));
-                        for (int j = 0; j <i+1; j++) {
-                            resultout.appendText(String.format("%s%.1f%s", "*(x-", tabx.get(j), ")"));
-                        }
-                }
+//                resultout.setText("W"+String.valueOf(tabx.size()-1)+"(x)=");
+//                resultout.appendText(String.valueOf(taby.get(0)));
+//                for(int i = 0; i<tabx.size()-1; i++) {
+//                    resultout.appendText(String.format("%s%.1f%s%d%s%.1f%s%d%s", "+(", subresult.get(i).get(0), "/(",i+1,"!*",h,"^",i+1,"))"));
+//                        for (int j = 0; j <i+1; j++) {
+//                            resultout.appendText(String.format("%s%.1f%s", "*(x-", tabx.get(j), ")"));
+//                        }
+//                }
             } catch (Exception exept) {
                 exept.printStackTrace();
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
